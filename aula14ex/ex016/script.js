486834e467ce91868aa4801c8bcb22468ecf9a6e
Icaro Leonel <103190983+icaroryann@users.noreply.github.com>
2025-06-02 05:57:17 -0300

    Atualizei script do ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,20 +3,24 @@ function contar(){
     let intInicio = Number(inicio.value)
 
     let fim = document.querySelector('#ifim').value
-    let intFim = Number(fim)
+    let intFim = Number(fim.value)
 
     let passo = document.querySelector('#ipasso')
-    let intPasso = parseInt(passo.value)
+    let intPasso = Number(passo.value)
 
     let resultado = document.querySelector('#resultado')
 
-    resultado.innerHTML = ''
+    resultado.innerHTML = 'Contando:<br>'
 
-    for (let i = intInicio; i <= intFim; i += intPasso) {
-        if (i < intFim) {
+    if (intInicio == 0 || intFim == 0) {
+        resultado.innerHTML = 'Impossível contar!'
+    } else if (intPasso == 0) {
+        window.alert('Passo inválido! Considerando PASSO 1')
+        intPasso = 1
+    } else {
+        for (let i = intInicio; i <= intFim; i += intPasso) {
             resultado.innerHTML += `${i} &#x1F449 `
-        } else {
-            resultado.innerHTML += i
         }
+        resultado.innerHTML += `&#x1F3F4`
     }
 }
