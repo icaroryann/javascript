a40423708edf0f4bc67439d49b049e8aec3cb189
Icaro Leonel <103190983+icaroryann@users.noreply.github.com>
2025-06-02 12:17:00 -0300

    atualização do ex016

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,26 +1,39 @@
 function contar(){
+    // Coleta dos inputs
     let inicio = document.querySelector('#iinicio')
-    let intInicio = Number(inicio.value)
-
-    let fim = document.querySelector('#ifim').value
-    let intFim = Number(fim.value)
-
+    let fim = document.querySelector('#ifim')
     let passo = document.querySelector('#ipasso')
-    let intPasso = Number(passo.value)
-
     let resultado = document.querySelector('#resultado')
 
-    resultado.innerHTML = 'Contando:<br>'
+    // Conversão das strings em valores inteiros
+    let intInicio = Number(inicio.value)
+    let intFim = Number(fim.value)
+    let intPasso = Number(passo.value)
 
-    if (intInicio == 0 || intFim == 0) {
+    // Verificação dos valores de inicio e fim se são iguais a zero ou vazios
+    if (inicio.value.length == 0 || fim.value.length == 0) {
         resultado.innerHTML = 'Impossível contar!'
-    } else if (intPasso == 0) {
-        window.alert('Passo inválido! Considerando PASSO 1')
-        intPasso = 1
     } else {
-        for (let i = intInicio; i <= intFim; i += intPasso) {
-            resultado.innerHTML += `${i} &#x1F449 `
+        // Caso o valor do passo seja 0 ou vazio, convertê-lo em 1.
+        if (passo.value.length == 0 || intPasso == 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+
+            intPasso = 1
+        }
+        resultado.innerHTML = 'Contando:<br>'
+
+        if (intInicio < intFim){
+            // Caso o valor seja crescente
+            for (let i = intInicio; i <= intFim; i += intPasso) {
+                resultado.innerHTML += `${i} &#x1F449 `
+            }
+        } else {
+            // Caso o valor seja regressivo
+            for (let i = intInicio; i >= intFim; i -= intPasso) {
+                resultado.innerHTML += `${i} &#x1F449 `
+            }
         }
         resultado.innerHTML += `&#x1F3F4`
+        
     }
 }
